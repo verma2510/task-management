4989caac76b9c083b7bef45063c32f4a1b7adc75
Aman <verma.aman1008@gmail.com>
2025-12-25 21:40:16 +0530

    remove hardcoded backend api

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import axios from 'axios'
 import './index.css'
+
+// Set Key - Value Pair
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
